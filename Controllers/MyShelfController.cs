76c81ce0dabf1b144650f31dd8b211555ffc9764
SmeetYadao <142222164+SmeetYadao@users.noreply.github.com>
2023-09-03 16:41:38 +0530

    Add files via upload

---
+++ Controllers/MyShelfController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorm_C_.Controllers
+{
+    public class MyShelfController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repositories/IAttributeMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookWorm_C_.Entities;
+using BookWormC.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorm_C_.Repositories
+{
+    public interface IAttributeMaster
+    {
+        Task<ActionResult<IEnumerable>> getAllAttribute();
+        Task<ActionResult> addAttribute(AttributeMaster a);
+    }
+}
