09b83c7d3b4df880d83187326b725f334ab3898e
SmeetYadao <142222164+SmeetYadao@users.noreply.github.com>
2023-09-03 16:56:32 +0530

    Update MyShelfController.cs

--- BookWorm-C#/Controllers/MyShelfController.cs
+++ BookWorm-C#/Controllers/MyShelfController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using BookWorm_C_.Entities;
+using BookWorm_C_.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWorm_C_.Controllers
 {
-    public class MyShelfController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyShelfController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IMyShelf _repository;
+
+        public MyShelfController(IMyShelf repository)
         {
-            return View();
+            _repository = repository;
         }
+
+        [HttpGet("getShelfById/{id}")]
+        public async Task<ActionResult<MyShelf>> getShelfById(long MyShelfId)
+        {
+            var MyShelf = await _repository.getById(MyShelfId);
+            if (MyShelf == null)
+            {
+                return NotFound();
+            }
+            return Ok(MyShelf);
+        }
+
+        [HttpGet("getShelfByCustId/{CustId}")]
+        public async Task<ActionResult<MyShelf>> GetShelfByCustId(long CustomerId)
+        {
+            var MyShelf = await _repository.getByCustomerId(CustomerId);
+            if (MyShelf == null)
+            {
+                return NotFound();
+            }
+            return Ok(MyShelf);
+        }
+
     }
 }
