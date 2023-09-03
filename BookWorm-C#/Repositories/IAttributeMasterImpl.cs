5e86b13124018d398d2f4ed3fc6010d8777a6994
SmeetYadao <142222164+SmeetYadao@users.noreply.github.com>
2023-09-03 15:59:53 +0530

    Add files via upload

---
+++ BookWorm-C#/Repositories/IAttributeMasterImpl.cs
@@ -0,0 +1,37 @@
+﻿using BookWorm_C_.Repositories;
+using BookWormC.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using WebApplication1.Models;
+
+
+namespace BookWormC.Repositories
+{
+    public class IAttributeMasterImpl : IAttributeMaster
+    {
+        private readonly Appdbcontext context;
+
+
+        public IAttributeMasterImpl(Appdbcontext context)
+        {
+            this.context = context;
+        }
+        async Task<ActionResult> IAttributeMaster.addAttribute(AttributeMaster a)
+        {
+            context.AttributeMaster.Add(a);
+            await context.SaveChangesAsync();
+            return a;
+        }
+
+        async Task<ActionResult<IEnumerable>> IAttributeMaster.getAllAttribute()
+        {
+            if (context.AttributeMaster == null)
+            {
+                return null;
+            }
+            return await context.AttributeMaster.ToListAsync();
+        }
+    }
+
+}
