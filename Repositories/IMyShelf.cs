8997196a81c01674e00d0c34a777b941b7fc3a2a
SmeetYadao <142222164+SmeetYadao@users.noreply.github.com>
2023-09-03 16:48:48 +0530

    Update IMyShelf.cs

--- Repositories/IMyShelf.cs
+++ Repositories/IMyShelf.cs
@@ -1,6 +1,11 @@
-﻿namespace BookWorm_C_.Repositories
+using BookWorm_C_.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorm_C_.Repositories
 {
     public interface IMyShelf
     {
+        Task<ActionResult<MyShelf>?> getById(long MyShelfId);
+        Task<ActionResult<MyShelf>?> getByCustomerId(long CustomerId);
     }
 }
